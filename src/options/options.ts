7ad2811659acf526ccb4d23576e031f52b088bb4
DatGuy <datguysteam@gmail.com>
2026-01-26 08:05:43 +0200

    simplify spiHelperSettings initialization using default settings

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,60 +1,10 @@
 // Validator type system
-import { spiHelperIsCheckuser } from '../role.ts';
 import { spiHelperGetAPI } from '../api.ts';
 import type { ScriptSettings } from './types.ts';
 import { migrateSettings } from './migration.ts';
+import { spiHelperDefaultSettings } from '../constants/settings.ts';
 
-export const spiHelperSettings: ScriptSettings = {
-  // Choices are 'watch' (unconditionally add to watchlist), 'preferences'
-  // (follow default preferences), 'nochange' (don't change the watchlist
-  // status of the page), and 'unwatch' (unconditionally remove)
-  watch: {
-    case: 'preferences',
-    archive: 'nochange',
-    tagged: 'preferences',
-    categories: 'nochange',
-    blocked: true,
-  },
-  expiry: {
-    case: 'indefinite',
-    archive: 'indefinite',
-    tagged: 'indefinite',
-    categories: 'indefinite',
-    blocked: 'indefinite',
-  },
-  // Log all actions to log.page
-  log: {
-    enabled: false,
-    reversed: false,
-    page: 'spihelper_log',
-  },
-  // Lets people disable clerk options if they're not a clerk
-  clerk: true,
-  // Enable the "move section" button
-  iUnderstandSectionMoves: false,
-  // Automatically tick the "Archive case" option if the case is closed
-  tickArchiveWhenCaseClosed: true,
-  // Use checkuserblock-account when CU blocking. False when not a CU, by default true when a CU
-  useCheckuserblockAccount: spiHelperIsCheckuser(false),
-  // Default IPv6 listings to /64 in the block/tag socks menu
-  displayIPv6As64: true,
-  // Whether to lookup usernames and pages while writing in the menu
-  useLookup: true,
-  interface: {
-    // Should we pin the top view to the top, or make it sticky
-    pinned: true,
-    // Whether to use the button layout (true) or the accordion layout (false)
-    buttonLayout: true,
-  },
-  // These are for debugging to view as other roles. If you're picking apart the code and
-  // decide to set these (especially the CU option), it is YOUR responsibility to make sure
-  // you don't do something that violates policy
-  debug: {
-    enabled: false,
-    forceCheckuser: false,
-    forceAdmin: false,
-  },
-};
+export const spiHelperSettings: ScriptSettings = structuredClone(spiHelperDefaultSettings);
 
 const saveKey = 'userjs-spihelper';
 
