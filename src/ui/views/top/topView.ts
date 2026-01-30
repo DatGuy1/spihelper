39e764eaa7bd46cc020f85e4e96ed4e3d5f772ee
DatGuy <datguysteam@gmail.com>
2026-01-30 19:20:58 +0200

    pass state to submit form
    Whoops

--- src/ui/views/top/topView.ts
+++ src/ui/views/top/topView.ts
@@ -203,7 +203,7 @@ export const TopViewComponent = defineComponent({
       <submit-form v-if="caseActions.sections.data.section !== null" v-model:socks="caseActions.block.data.accounts"
                    v-model:master="caseActions.block.data.master" v-model:altmaster="caseActions.block.data.altmaster"
                    v-model:lock-comment="caseActions.block.data.lockcomment" :locks="caseActions.block.data.userlocks"
-                   :all-disabled="allDisabled"
+                   :all-disabled="allDisabled" :state="state"
                    @on-submit="onSubmitActions" />
       <cdx-progress-bar v-if="actionsRunning" aria-label="Actions in progress" style="margin-top: 20px;" />
       <div id="messageRow">
