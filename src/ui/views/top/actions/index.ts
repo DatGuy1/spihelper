fde53d3c3640a300e72af3d9f99c3f19c85d26cd
DatGuy <datguysteam@gmail.com>
2026-02-15 13:27:22 +0000

    move some more files into index.ts

--- src/ui/views/top/actions/index.ts
+++ src/ui/views/top/actions/index.ts
@@ -1,5 +1,7 @@
+export { ArchiveActionComponent } from './archiveAction.ts';
+export { BlockActionComponent } from './blockAction.ts';
 export { CommentActionComponent } from './commentAction.ts';
 export { ChangeStatusActionComponent } from './changeStatusAction.ts';
-export { BlockActionComponent } from './blockAction.ts';
 export { LinkActionComponent } from './linkAction.ts';
 export { ManagementActionComponent } from './managementAction.ts';
+export { MoveActionComponent } from './moveAction.ts';
--- src/ui/views/top/index.ts
+++ src/ui/views/top/index.ts
@@ -1,5 +1,6 @@
 export { ActionAccordionComponent } from './actionAccordion';
 export { ActionButtonComponent } from './actionButton';
 export { ActionContainerComponent } from './actionContainer.ts';
+export { ActionContentComponent } from './actionContent.ts';
 export { TopViewComponent } from './topView';
 export * from './actions';
